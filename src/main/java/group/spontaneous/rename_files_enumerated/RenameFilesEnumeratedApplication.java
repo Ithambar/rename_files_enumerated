f8f16d033cb057434f7fdcc17c94494a54763da5
Ithambar <drache.dragon@gmail.com>
2020-04-25 14:25:07 +0200

    rename project; remove unnecessary files

---
+++ src/main/java/group/spontaneous/rename_files_enumerated/RenameFilesEnumeratedApplication.java
@@ -0,0 +1,14 @@
+package group.spontaneous.rename_files_enumerated;
+
+import java.util.ArrayList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenameFilesEnumeratedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenameFilesEnumeratedApplication.class, args);
+	}
+}
