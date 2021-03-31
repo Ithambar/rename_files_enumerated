e8fec669a91fe003cbc8ba022ad6ba3c2bd8ae4f
G.Felix <felix.grafschmidt@gmail.com>
2021-03-31 20:06:42 +0200

    pad number

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const fs = require("fs")
+const fs = require("fs");
 const path = require("path")
 
 const moveFrom = "./input";
@@ -12,9 +12,11 @@ if (cliParams.length > 0) {
 }
 
 (async () => {
-	fs.rmdir(moveTo, { recursive: true }, () => {
-		fs.mkdir(moveTo, async () => {
-			try {
+	try {
+		fs.rmdir(moveTo, {
+			recursive: true
+		}, () => {
+			fs.mkdir(moveTo, async () => {
 				// Get the files as an array
 				const files = await fs.promises.readdir(moveFrom);
 
@@ -23,18 +25,18 @@ if (cliParams.length > 0) {
 				for (const file of files) {
 					// Get the full paths
 					const fromPath = path.join(moveFrom, file);
-					const toPath = path.join(moveTo, filename + (i++).toString());
+					const toPath = path.join(moveTo, `${(i++).toString().padStart(4, "0")}_${filename}${path.extname(file)}`);
 
 					// Now move async
 					await fs.promises.copyFile(fromPath, toPath);
 				} // End for...of
-			} catch (e) {
-				// Catch anything bad that happens
-				console.error("We've thrown! Whoops!", e);
-			}
+
+			})
+
 		})
-
-	})
-	// Our starting point
-
+		// Our starting point
+	} catch (e) {
+		// Catch anything bad that happens
+		console.error("We've thrown! Whoops!", e);
+	}
 })(); // Wrap in parenthesis and call now
